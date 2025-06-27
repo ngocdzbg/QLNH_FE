a03253747743b9d009ae4874e4c81bcd106666f3
ngocnb <mrbaobg@gmail.com>
2025-06-27 16:35:06 +0700

    init project and config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ThucDonComponent } from './thuc-don/thuc-don.component';
+import { DsTDonComponent } from './thuc-don/ds-t-don/ds-t-don.component';
+import { ThemSuaTDonComponent } from './thuc-don/them-sua-t-don/them-sua-t-don.component';
+import { MonAnComponent } from './mon-an/mon-an.component';
+import { DsMAnComponent } from './mon-an/ds-m-an/ds-m-an.component';
+import { ThemSuaMAnComponent } from './mon-an/them-sua-m-an/them-sua-m-an.component';
+
+import { SharedService } from './shared.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThucDonComponent,
+    DsTDonComponent,
+    ThemSuaTDonComponent,
+    MonAnComponent,
+    DsMAnComponent,
+    ThemSuaMAnComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
